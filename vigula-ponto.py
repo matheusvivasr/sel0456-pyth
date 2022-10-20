920618d5390a84ebf3a43e545e01afc186cb3deb
matheusvivasr <vivas.matheus@usp.br>
2022-10-20 15:28:35 -0300

    Ler e editar arquivo trocando virgulas

--- vigula-ponto.py
+++ vigula-ponto.py
@@ -1,19 +1,27 @@
-with open("arq.txt", "r") as entrada:
-    caracteres = entrada.read()
+def substituir(texto):
+    modificado = ","
 
-dentro=0
-virgulas=0
-for caracter in caracteres:
-    if caracter == "\"" and not dentro:
-        dentro = 1
-    elif caracter == "\"" and dentro:
-        dentro = 0
+    dentro = False
+    frase = list(texto)
 
-    if caracter==",":
-        virgulas+=1
-    
-    if caracter =="," and not dentro:
+    for indice in range(len(frase)):        
+        if frase[indice] == '"' and not dentro:
+            dentro = True
+        elif indice == '"' and dentro:
+            dentro = False
         
+        if frase[indice] == modificado and not dentro:
+            frase[indice] = ';'
+
+    out = ''.join(frase)
+
+    return out
+
+
+with open("in.txt", "r") as entrada:
+    caracteres = entrada.read()
+        
+texto_modificado = substituir(caracteres)
 
 with open("out.txt", "w") as saida:
-    saida.write(caracteres)
+    saida.write(texto_modificado)
